ca557599aa7819a3bafb55aa73343182bd28b524
Rob Harkness <rob.harkness.dev@gmail.com>
2020-01-14 14:53:19 +0100

    update js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,8 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
+const changeH2Casing = () => {
 	const moduleNameRegex = /(qctrl)|\./gi;
-	const h2ModuleNames = document.querySelectorAll("h2").filter(element => {
+	const h2elements = document.querySelectorAll("h2");
+	const h2ModuleTitles = [...h2elements].filter(element => {
 		const elText = element.innerText;
 		return moduleNameRegex.test(elText);
 	});
-	console.log(h2ModuleNames);
+
+	h2ModuleTitles.forEach(
+		element => (element.style.textTransform = "uppercase")
+	);
+};
+
+const changeBreadcrumbsSeparator = () => {
+	const breadCrumbsEl = document.querySelector(".wy-breadcrumbs");
+
+	[...bcs.children].forEach(el => {
+		const newHTML = el.innerHTML
+			.split(" ")
+			.map(v => (v === "»" ? ">" : v))
+			.join(" ");
+
+		el.innerHTML = newHTML;
+	});
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	changeH2Casing();
+	changeBreadcrumbsSeparator();
 });
