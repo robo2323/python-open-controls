a84f90c5737b722627676fb31c664d4db2a16067
Rob Harkness <rob.harkness.dev@gmail.com>
2020-01-14 12:43:34 +0100

    custom js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,1 +1,8 @@
-console.log("works!");
+document.addEventListener("DOMContentLoaded", () => {
+	const moduleNameRegex = /(qctrl)|\./gi;
+	const h2ModuleNames = document.querySelectorAll("h2").filter(element => {
+		const elText = element.innerText;
+		return moduleNameRegex.test(elText);
+	});
+	console.log(h2ModuleNames);
+});
