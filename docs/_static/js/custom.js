dd15d958d12f8504ae6e530a8873bcb01eead161
Rob Harkness <rob.harkness.dev@gmail.com>
2020-01-20 14:42:53 +0100

    css flash fix

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,68 +1,73 @@
-const loadCSS = () => {
+"use strict";
+
+document.body.style.display = "none";
+
+var loadCSS = function loadCSS() {
 	// Check if we are at dev or prod docs url and load appropriate CSS
-	const cssUrl = /dev/i.test(window.location.hostname)
+	var cssUrl = /dev/i.test(window.location.hostname)
 		? "https://dev.docs.q-ctrl.com/assets/css/sphinx.css"
 		: "https://docs.q-ctrl.com/assets/css/sphinx.css";
-
-	const head = document.getElementsByTagName("head")[0];
-	const link = document.createElement("link");
+	var head = document.getElementsByTagName("head")[0];
+	var link = document.createElement("link");
 	link.rel = "stylesheet";
 	link.type = "text/css";
 	link.href = cssUrl;
 	link.media = "all";
-	head.appendChild(link);
+
+	link.onload(() => {
+		head.appendChild(link);
+	});
 };
 
-const changeH2Casing = () => {
-	const moduleNameRegex = /(qctrl)|\.|_/i;
-	const h2elements = document.querySelectorAll("h2, h3");
-	const h2ModuleTitles = [...h2elements].filter(element => {
-		const elText = element.innerText;
+var changeH2Casing = function changeH2Casing() {
+	var moduleNameRegex = /(qctrl)|\.|_/i;
+	var h2elements = document.querySelectorAll("h2, h3");
+	var h2ModuleTitles = [].concat(h2elements).filter(function(element) {
+		var elText = element.innerText;
 		return moduleNameRegex.test(elText);
 	});
-
-	h2ModuleTitles.forEach(
-		element => (element.style.textTransform = "lowercase")
-	);
-};
-
-const changeBreadcrumbsSeparator = () => {
-	const breadCrumbsEl = document.querySelector(".wy-breadcrumbs");
-
-	[...breadCrumbsEl.children].forEach(el => {
-		const newHTML = el.innerHTML
-			.split(" ")
-			.map(v => (v === "»" ? ">" : v))
-			.join(" ");
-
-		el.innerHTML = newHTML;
+	h2ModuleTitles.forEach(function(element) {
+		return (element.style.textTransform = "lowercase");
 	});
 };
 
-const fixNavHeader = () => {
-	const imageEl = document.querySelector(
+var changeBreadcrumbsSeparator = function changeBreadcrumbsSeparator() {
+	var breadCrumbsEl = document.querySelector(".wy-breadcrumbs");
+
+	var newHTML = breadCrumbsEl.innerHTML
+		.split("")
+		.map(function(v) {
+			return v === "»" ? ">" : v;
+		})
+		.join("");
+
+	breadCrumbsEl.innerHTML = newHTML;
+};
+
+var fixNavHeader = function fixNavHeader() {
+	var imageEl = document.querySelector(
 		".wy-side-nav-search > a.icon img.logo"
 	);
-	const navHeaderEl = document.querySelector(".wy-side-nav-search > a");
+	var navHeaderEl = document.querySelector(".wy-side-nav-search > a");
 	navHeaderEl.innerHTML = "";
 	navHeaderEl.appendChild(imageEl);
 };
 
-const styleMobileNavHeader = () => {
-	const navLogo = document.createElement("img");
+var styleMobileNavHeader = function styleMobileNavHeader() {
+	var navLogo = document.createElement("img");
 	navLogo.src = "_static/logo.svg";
 	navLogo.className = "logo";
 	navLogo.alt = "Logo";
-
-	const headerLinkEl = document.querySelector(".wy-nav-top a");
+	var headerLinkEl = document.querySelector(".wy-nav-top a");
 	headerLinkEl.innerHTML = "";
 	headerLinkEl.appendChild(navLogo);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-	// loadCSS();
+document.addEventListener("DOMContentLoaded", function() {
+	loadCSS();
 	changeH2Casing();
 	changeBreadcrumbsSeparator();
 	fixNavHeader();
 	styleMobileNavHeader();
+	document.body.style.display = block;
 });
